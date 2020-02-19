ae74e6cbabae5ab53cfcb9cce4abbf3003058124
Jun Kwan <kwanjunwen@gmail.com>
2020-02-19 15:31:14 +1100

    Minor bug fix.

--- run_inference.py
+++ run_inference.py
@@ -38,12 +38,16 @@ if __name__ == '__main__':
         cv2_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
         predictions, bounding_box, face_keypoints, w = head_pose_estimation(cv2_frame, mtcnn, model, transformations, softmax, idx_tensor)
-        # Print new frame with cube and axis
-        utils.draw_axis(frame, predictions[0], predictions[1], predictions[2], tdx = (face_keypoints[0] + face_keypoints[2]) / 2, tdy= (face_keypoints[1] + face_keypoints[3]) / 2, size = w/2)
 
-        cv2.rectangle(frame,(bounding_box[0], bounding_box[1]), \
-                      (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]), \
-                      (140,255,255), 3)
+        # Print new frame with cube and axis
+        for i in range(len(predictions)):
+
+            utils.draw_axis(frame, predictions[i][0], predictions[i][1], predictions[i][2], \
+                            tdx = (face_keypoints[i][0] + face_keypoints[i][2]) / 2, tdy= (face_keypoints[i][1] + face_keypoints[i][3]) / 2, size = w[i]/2)
+
+            cv2.rectangle(frame,(bounding_box[i][0], bounding_box[i][1]), \
+                          (bounding_box[i][0]+bounding_box[i][2], bounding_box[i][1] + bounding_box[i][3]), \
+                          (140,255,255), 3)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
